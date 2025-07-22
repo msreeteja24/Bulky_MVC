60402f0b7c769ddb9ab5d1a96234eee94bb61e00
msreeteja24 <msreeteja24@gmail.com>
2025-07-22 13:42:13 +0530

    Seed customer data in the Categories table

---
+++ BulkyWeb/Migrations/20250722080123_SeedDataIntoCategory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BulkyWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedDataIntoCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Insert into Categories(Name,DisplayOrder) Values('Action',1)");
+            migrationBuilder.Sql("Insert into Categories(Name,DisplayOrder) Values('SciFi',2)");
+            migrationBuilder.Sql("Insert into Categories(Name,DisplayOrder) Values('History',3)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Categories WHERE Id = 1;");
+            migrationBuilder.Sql("DELETE FROM Categories WHERE Id = 2;");
+            migrationBuilder.Sql("DELETE FROM Categories WHERE Id = 3;");
+        }
+    }
+}
